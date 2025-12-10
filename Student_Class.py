1156372cf01510ed936d37e7827b5cc4a6dd0470
FaustinNdayishimiye <faustinnet12@gmail.com>
2025-12-10 16:06:29 +0200

    new

--- Student_Class.py
+++ Student_Class.py
@@ -4,9 +4,20 @@ class Student:
        self.age =age
        self.grade = grade
        
-    def get_info (self):
-        print(self.name, self.age , self.grade)
+    def get_info (self): #METHODS
+        return f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}"
         
-         
-        X= Student("GAD", "12" , "40")
-        X.get_info()
+    
+#CREATING OBJ
+Student1= Student("Fred", 7,"A" )
+Student2= Student ("Frank", 18, "C")
+Student3= Student("X", "Y", "Z")
+        
+        
+    #printing info of all student 
+print(Student1.get_info())
+print(Student2.get_info())  
+print(Student3.get_info())
+
+   
+   
